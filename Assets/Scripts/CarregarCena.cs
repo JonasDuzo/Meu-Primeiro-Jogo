949e11a8577492631510bd624c168cf1f239f4e6
Jonas Duzo <jonas10duzo@gmail.com>
2019-01-20 15:00:23 -0200

    Primeiro Commit

---
+++ Assets/Scripts/CarregarCena.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarregarCena : MonoBehaviour
+{
+    public void CarregarProxCena()
+    {
+        int carregarCena = SceneManager.GetActiveScene().buildIndex; //Variavel para construir a construir a cena
+        SceneManager.LoadScene(carregarCena + 1); //Carregar proxima cena
+    }
+    
+    public void Inicio()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Sair()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI proxPalpite;
+    int palpite;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        ProxPalpite();
+    }
+
+    public void OnClickMais()
+    {
+        min = palpite;
+        ProxPalpite();
+    }
+
+    public void OnClickMenos()
+    {
+        max = palpite;
+        ProxPalpite();
+    }
+
+    public void ProxPalpite()
+    {
+        palpite = Random.Range(min, max +1);
+        proxPalpite.text = palpite.ToString();// Dizendo que os valores que o campo proximo palpite receber vai ser convertido para String;
+    }
+}
